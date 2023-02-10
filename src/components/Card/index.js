45f7e92d6961ce5b755dca3d00f98c4065060b5b
LeozinhoPortoM <leo.porto.lpm@hotmail.com>
2023-02-09 22:29:11 -0300

    include card animation and setting button remove

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,10 +1,14 @@
 import "./style.css";
 
-export default function Card({ product }) {
+export default function Card({ product, handleRemove }) {
   return (
     <div className="card">
       <h1>{product.name}</h1>
       <h4>R$: {product.price}</h4>
+      <div className="card-button">
+        <button>Editar</button>
+        <button onClick={() => handleRemove(product.id)}>Excluir</button>
+      </div>
     </div>
   );
 }
